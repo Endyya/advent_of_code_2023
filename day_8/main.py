bd1461d79c8d763eb63959e0cabbded59a4d31aa
Endyya <endya@protonmail.com>
2023-12-11 00:09:45 +0100

    Finished day 8 part 1

--- day_8/main.py
+++ day_8/main.py
@@ -1,3 +1,32 @@
+import itertools as itt
 
-with open('test') as f:
-    pass
+code = {}
+with open('input') as f:
+    loop = f.readline().rstrip()
+
+    line = f.readline()
+    line = f.readline().rstrip()
+
+    while line != '':
+        parsing = line.split('=')
+        parsing[1] = parsing[1].replace(')', '')
+        parsing[1] = parsing[1].replace('(', '')
+        parsing[1] = parsing[1].replace(' ', '')
+        code[parsing[0].strip()] = parsing[1].split(',')
+        line = f.readline().rstrip()
+
+position = 'AAA'
+
+counter = 0
+for instruction in itt.cycle(loop):
+    if position == 'ZZZ':
+        break
+    else:
+        counter += 1
+        if instruction == 'L':
+            position = code[position][0]
+        else:
+            position = code[position][1]
+
+print('part 1 : ', counter)
+    
